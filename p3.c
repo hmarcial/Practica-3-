9d2386c60f6f3c81938cb35ff98ab51391379acf
Hector Marcial <48300532+hmarcial@users.noreply.github.com>
2019-05-30 19:16:33 -0500

    Practica 3 Casi Final
    Practica Funcional Faltan Algunas cosas por modificar

--- p3.c
+++ p3.c
@@ -1,38 +1,79 @@
-#include <libreria.h>
+#include <Mate.h>
+void clean(void);
+void total(void);
+char cadena[50];
+float result = 0;
+float result2 = 0;
+int h=0;
+int flagRecibido = 0;
+int Caso = 0;
+#INT_RDA
+void serial_isr(){
+  if(kbhit()){
+      cadena[h]=getch();  
+      flagRecibido=1;
+  }
+}
 void main (void){
-    printf("\fIngresa los datos=\n");
+    enable_interrupts(global);
+    enable_interrupts(int_rda);
+    printf("Ingresa los datos de la siguiente manera numero 1 enter numero 2 enter operacion=  \n");
     while(TRUE){
-    if(kbhit()){
-    if(caluclo ==1){
-        Numero2[j]=getch();
-        if(Numero2[j]>=42 && Numero2[j]<=62 || Numero2[j]==13){
-            printf("%c",Numero2[j]);
-            if(Numero2[j]==13){
-                calculo=2;
+      if(flagRecibido==1){
+         flagRecibido=0;
+        
+        switch(Caso){
+          case 0:
+            if(cadena[h]>=48 && cadena[h]<=57 || cadena[h]==43 || cadena[h] ==45 || cadena[h] ==46 ){
+               printf("%c",cadena[h]);
+               h++;
             }
-            j++;
-            if(calculo==2){
-            operacion[k] = getch();
-            if(operacion[k]==42 || operacion[k]==43 || operacion[k]==45 || operacion[k] ==47){
-            printf("%c",operacion[k]);
-            result = atof(Numero);
-            result2 = atof(Numero2);
-            dale();
-            k++;
+             if(cadena[h] == 13){
+                 result=atof(cadena);
+                 printf("\n\r");
+                 printf("%f",result);
+                 printf("\n\r");
+                 clean();
+                 Caso = 1;}
+         break;
+         case 1:
+            if(cadena[h]>=48 && cadena[h]<=57 || cadena[h]==43 || cadena[h] ==45 || cadena[h] ==46 ){
+               printf("%c",cadena[h]);         
+               h++;
             }
+            if(cadena[h] == 13){
+               result2=atof(cadena);
+               printf("\n\r");
+               printf("%f",result2);
+               printf("\n\r");
+               clean();
+               Caso = 2;
             }
-        }
-    
-    }else{
-        Numero[i]=getch();
-        if(Numero[i]>=42 && Numero[i]<=62 || Numero[i]==13){
-            printf("%c",Numero[i]);
-            if(Numero[i]==13){
-            caluclo=1;
-        }
-        i++;
-        }
+         break;
+         case 2:
+            if(cadena[h]==42 || cadena[h]==43 || cadena[h]==45 || cadena[h] ==47){
+               printf("%c",cadena[h]);
+               operaciones(cadena,h,result,result2);
+            }
+            clean();
+            total();
+         break;
+         
+        }        
     }
-   }
-   }
+
+  }
+}
+void clean(void){
+      for(int c=0; c<=h;c++){
+          if(cadena[c]!=NULL){
+              cadena[c]=NULL;
+         }
+      }
+      h=0;
+}
+void total(void){
+   result = 0;
+   result2 = 0;
+   Caso = 0;
 }
