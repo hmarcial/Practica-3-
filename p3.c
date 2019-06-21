99378b353d883e9e9b3eec353df501f4f3524e6f
Hector Marcial <48300532+hmarcial@users.noreply.github.com>
2019-06-20 19:23:12 -0500

    Practica 3 Final
    Arregla erro de resultado en terminal

--- p3.c
+++ p3.c
@@ -2,11 +2,13 @@
 void clean(void);
 void total(void);
 char cadena[50];
+char operador;
 float resultado = 0;
 float resultado2 = 0;
 int puntero=0;
 int flagRecibido = 0;
 int Caso = 0;
+int MensajeInicio =1;
 #INT_RDA
 void serial_isr(){
   if(kbhit()){
@@ -18,27 +20,13 @@ void serial_isr(){
 void main (void){
     enable_interrupts(global);
     enable_interrupts(int_rda);
-    printf("Ingresa los datos de la siguiente manera numero 1 enter numero 2 enter operacion=  \n");
     while(TRUE){
+      if(MensajeInicio==1){
+         MensajeInicio=0;
+         printf("Ingresa los datos de la siguiente manera numero 1 enter numero 2 enter operacion=  \n");
+      }
       if(flagRecibido==1){
-         flagRecibido=0;
-        /*if(cadena[h-1] == 13){
-        
-            if(Caso==0){
-                  result=atof(cadena);
-                  printf("\n\r");
-                  printf("%f",result);
-                  printf("\n\r"); 
-            }
-            if(Caso==1){
-                  result2=atof(cadena);
-                  printf("\n\r");
-                  printf("%f",result2);
-                  printf("\n\r");                   
-            }
-            Caso++;
-            clean();
-        }*/        
+         flagRecibido=0;      
         switch(Caso){
           case 0:
             if(cadena[puntero-1]>=48 && cadena[puntero-1]<=57 || cadena[puntero-1]==43 || cadena[puntero-1] ==45 || cadena[puntero-1] ==46 ){
@@ -69,16 +57,20 @@ void main (void){
          case 2:
             if(cadena[puntero-1]==42 || cadena[puntero-1]==43 || cadena[puntero-1]==45 || cadena[puntero-1] ==47){
                printf("%c",cadena[puntero-1]);
-               operaciones(cadena,puntero-1,resultado,resultado2);
-            }else{
-               printf("Error No Numero en esta parte");
-               getch();
-               printf("\f");
+               operador = cadena[puntero-1];
             }
+            if(cadena[puntero-1] ==13){
+               printf("\n\r");
+               clean();
+               Caso =3;
+            }
+         break;
+         case 3:
+            printf("El resultador es: ");
+            operaciones(operador,resultado,resultado2);
             clean();
             total();
-         break;
-         
+            MensajeInicio=1;
         }        
     }
 
@@ -96,4 +88,5 @@ void total(void){
    resultado = 0;
    resultado2 = 0;
    Caso = 0;
+   operador=NULL;
 }
